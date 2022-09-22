05699d7486a69d9dc15a872f9d8eceb1e6ce2516
yathish <yathishkumar.satheesh@softsuave.com>
2022-09-22 13:03:22 +0530

    Generic product create view

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ProductsListView, ProductDetailView
+from .views import ProductsListView, ProductDetailView, ProductCreateView
 
 urlpatterns = [
     path('', ProductsListView.as_view(), name='products_list'),
     path('<int:pk>/details', ProductDetailView.as_view(), name='product_detail'),
+    path('create', ProductCreateView.as_view(), name='product_create'),
 ]
